846047bb5c19b834fd5650d8198c0d87c6dd2e6d
João Pereira <joaopereira_19@hotmail.com>
2017-06-09 00:15:35 +0100

    completei dao de alimentos

--- Mnham-Mnham/AlimentoDAO.cs
+++ Mnham-Mnham/AlimentoDAO.cs
@@ -9,43 +9,133 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Mnham_Mnham
 {
     class AlimentoDAO : DAO
     {
         ClassificacaoAlimentoDAO classificacoes;
-
-        internal List<Alimento> ObterIngredientesAlimentos(int idEstabelecimento, string v)
+/*
+        public List<Alimento> ObterIngredientesAlimentos(int idEstabelecimento, string v)
         {
             // construir lista de alimentos que na designação contenham v
-            // apenas se obtem id e ingredientes
-            throw new NotImplementedException();
-        }
+            SqlCommand cmd = new SqlCommand("Select id from Alimento WHERE id_estabelecimento = @id_est AND CHARINDEX(@v,designacao) > 0", base.sqlCon);
+            cmd.Parameters["@v"].Value = v;
+            cmd.Parameters["@id_est"].Value = idEstabelecimento;
+            var reader = cmd.ExecuteReader();
+            List<Alimento> ret = new List<Alimento>();
 
-        internal Alimento ObterAlimento(int idAlimento)
+            List<int> idsAlimentos = new List<int>();
+
+            while (reader.Read())
+            {
+                idsAlimentos.Add(Convert.ToInt32(reader["id"]));
+            }
+
+            foreach(var i in idsAlimentos)
+            {
+                SqlCommand cmd2 = new SqlCommand("select Ingrediente.designacao FROM Ingrediente INNER JOIN IngredienteAlimento ON IngredienteAlimento.id_alimento = Ingrediente.id WHERE Ingrediente.id = @id", base.sqlCon);
+                cmd2.Parameters["@id"].Value = i;
+                var reader2 = cmd2.ExecuteReader();
+                ISet<string> ing = new HashSet<string>();
+
+                while (reader2.Read())
+                {
+                    ing.Add(reader2["designacao"].ToString());
+                }
+
+                // apenas se obtem id e ingredientes
+                ret.Add(new Alimento(i, null, null, ing, null));
+            }
+
+            return ret;
+        }
+        */
+
+        public Alimento ObterAlimento(int idAlimento)
         {
-            throw new NotImplementedException();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Alimento WHERE id = @id AND removido == 0", base.sqlCon);
+            cmd.Parameters.Add("@id", SqlDbType.Int, 50);
+            cmd.Parameters["@id"].Value = idAlimento;
+            Alimento a = null;
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            bool contains = false;
+
+            try
+            {
+                contains = reader.Read();
+            }
+            finally
+            {
+                reader.Close();
+            }
+
+            if(contains)
+            {
+                SqlCommand cmd2 = new SqlCommand("select Ingrediente.designacao FROM Ingrediente INNER JOIN IngredienteAlimento ON IngredienteAlimento.id_alimento = Ingrediente.id WHERE Ingrediente.id = @id", base.sqlCon);
+                cmd2.Parameters["@id"].Value = idAlimento;
+                SqlDataReader reader2 = cmd2.ExecuteReader();
+                ISet<String> ingredientes = new HashSet<String>();
+
+                while (reader2.Read())
+                {
+                    ingredientes.Add(reader2["designacao"].ToString());
+                }
+
+                a = new Alimento(idAlimento, reader["designacao"].ToString(), (float) Convert.ToDecimal(reader["preco"]), ingredientes, (byte[]) reader["foto"]);
+            }
+
+            return a;
         }
 
-        internal bool ClassificarAlimento(int idAlimento, Classificacao cla)
+        public bool ClassificarAlimento(int idAlimento, Classificacao cla)
         {
             return classificacoes.ClassificarAlimento(idAlimento, cla);
         }
 
-        internal bool RemoverClassificacaoAlimento(int idAlimento, int clienteAutenticado)
+        public bool RemoverClassificacaoAlimento(int idAlimento, int clienteAutenticado)
         {
             return classificacoes.RemoverClassificacaoAlimento(idAlimento, clienteAutenticado);
         }
 
-        internal IEnumerable<Alimento> ObterAlimentos(string nomeAlimento)
+        public List<Alimento> ObterAlimentos(string nomeAlimento)
         {
-            // cada alimento apenas tem o id e os seus ingredientes
-            // obter alimentos que contenham 'nomeAlimento'
-            throw new NotImplementedException();
+            // construir lista de alimentos que na designação contenham nomeAlimento
+            SqlCommand cmd = new SqlCommand("Select id from Alimento WHERE CHARINDEX(@v,designacao) > 0", base.sqlCon);
+            cmd.Parameters["@v"].Value = nomeAlimento;
+            var reader = cmd.ExecuteReader();
+            List<Alimento> ret = new List<Alimento>();
+
+            List<int> idsAlimentos = new List<int>();
+
+            while (reader.Read())
+            {
+                idsAlimentos.Add(Convert.ToInt32(reader["id"]));
+            }
+
+            foreach (var i in idsAlimentos)
+            {
+                SqlCommand cmd2 = new SqlCommand("select Ingrediente.designacao FROM Ingrediente INNER JOIN IngredienteAlimento ON IngredienteAlimento.id_alimento = Ingrediente.id WHERE Ingrediente.id = @id", base.sqlCon);
+                cmd2.Parameters["@id"].Value = i;
+                var reader2 = cmd2.ExecuteReader();
+                ISet<string> ing = new HashSet<string>();
+
+                while (reader2.Read())
+                {
+                    ing.Add(reader2["designacao"].ToString());
+                }
+
+                // apenas se obtem id e ingredientes
+                ret.Add(new Alimento(i, null, null, ing, null));
+            }
+
+            return ret;
         }
 
-        internal List<Classificacao> ConsultarClassificacoesAlimentos(int clienteAutenticado)
+        public List<Classificacao> ConsultarClassificacoesAlimentos(int clienteAutenticado)
         {
             return classificacoes.ConsultarClassificacoesAlimentos(clienteAutenticado);
         }
