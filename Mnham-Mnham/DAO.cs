027ee324b5752288103a11af12c92ea27c8a4164
João Pereira <joaopereira_19@hotmail.com>
2017-06-09 17:50:48 +0100

    mudei DAO, acabada

--- Mnham-Mnham/DAO.cs
+++ Mnham-Mnham/DAO.cs
@@ -16,28 +16,6 @@ namespace Mnham_Mnham
 {
     class DAO
     {
-        // MUDAR IP, testar acesso à BD
-        private static string my_ip = "";
         public static string CONECTION_STRING = "Data Source=mnhammnham.database.windows.net;Initial Catalog = mnham-mnham; User ID = grupo3; Password=Passw0rd1";
-            
-            //"Data Source=" + my_ip + ";Network Library=DBMSSOCN;Initial Catalog = mnham-mnham;"; // User ID = myUsername; Password=myPassword;";
-            
-        
-
-        /* "Data Source=DESKTOP-3SNVUJ2;Initial Catalog=mnham-mnham;Integrated Security=True"; */
-        protected SqlConnection sqlCon;
-
-        /*
-        public DAO()
-        {
-            sqlCon = new SqlConnection(SQL_STRING);
-        }
-
-        public DAO(string connectionString)
-        {
-            sqlCon = new SqlConnection(connectionString);
-        }
-        */
-
     }
 }
