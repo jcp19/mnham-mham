a31b2ef186ef6f48bfea08bf98f5aa1c91eaaafc
João Pereira <joaopereira_19@hotmail.com>
2017-06-09 17:25:16 +0100

    update connection string

--- Mnham-Mnham/DAO.cs
+++ Mnham-Mnham/DAO.cs
@@ -18,7 +18,7 @@ namespace Mnham_Mnham
     {
         // MUDAR IP, testar acesso à BD
         private static string my_ip = "";
-        public static string CONECTION_STRING = "Data Source=mnhammnham.database.windows.net;Initial Catalog = mnham - mnham; User ID = grupo3; Password=Passw0rd1";
+        public static string CONECTION_STRING = "Data Source=mnhammnham.database.windows.net;Initial Catalog = mnham-mnham; User ID = grupo3; Password=Passw0rd1";
             
             //"Data Source=" + my_ip + ";Network Library=DBMSSOCN;Initial Catalog = mnham-mnham;"; // User ID = myUsername; Password=myPassword;";
             
