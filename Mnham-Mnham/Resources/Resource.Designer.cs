66e4ae83815c04cd764743a014f1841232de91a3
Fábio Baião <a75662@alunos.uminho.pt>
2017-06-17 17:18:29 +0100

    Adicionado novo logo.

--- Mnham-Mnham/Resources/Resource.Designer.cs
+++ Mnham-Mnham/Resources/Resource.Designer.cs
@@ -3164,76 +3164,79 @@ namespace Mnham_Mnham
 			public const int Icon = 2130837610;
 			
 			// aapt resource value: 0x7f02006b
-			public const int material_drawer_badge = 2130837611;
+			public const int Icon2 = 2130837611;
 			
 			// aapt resource value: 0x7f02006c
-			public const int material_drawer_circle_mask = 2130837612;
+			public const int material_drawer_badge = 2130837612;
 			
 			// aapt resource value: 0x7f02006d
-			public const int material_drawer_shadow_bottom = 2130837613;
+			public const int material_drawer_circle_mask = 2130837613;
 			
 			// aapt resource value: 0x7f02006e
-			public const int material_drawer_shadow_left = 2130837614;
+			public const int material_drawer_shadow_bottom = 2130837614;
 			
 			// aapt resource value: 0x7f02006f
-			public const int material_drawer_shadow_right = 2130837615;
+			public const int material_drawer_shadow_left = 2130837615;
 			
 			// aapt resource value: 0x7f020070
-			public const int material_drawer_shadow_top = 2130837616;
+			public const int material_drawer_shadow_right = 2130837616;
 			
 			// aapt resource value: 0x7f020071
-			public const int mic = 2130837617;
+			public const int material_drawer_shadow_top = 2130837617;
 			
 			// aapt resource value: 0x7f020072
-			public const int notification_action_background = 2130837618;
+			public const int mic = 2130837618;
 			
 			// aapt resource value: 0x7f020073
-			public const int notification_bg = 2130837619;
+			public const int notification_action_background = 2130837619;
 			
 			// aapt resource value: 0x7f020074
-			public const int notification_bg_low = 2130837620;
+			public const int notification_bg = 2130837620;
 			
 			// aapt resource value: 0x7f020075
-			public const int notification_bg_low_normal = 2130837621;
+			public const int notification_bg_low = 2130837621;
 			
 			// aapt resource value: 0x7f020076
-			public const int notification_bg_low_pressed = 2130837622;
+			public const int notification_bg_low_normal = 2130837622;
 			
 			// aapt resource value: 0x7f020077
-			public const int notification_bg_normal = 2130837623;
+			public const int notification_bg_low_pressed = 2130837623;
 			
 			// aapt resource value: 0x7f020078
-			public const int notification_bg_normal_pressed = 2130837624;
+			public const int notification_bg_normal = 2130837624;
 			
 			// aapt resource value: 0x7f020079
-			public const int notification_icon_background = 2130837625;
-			
-			// aapt resource value: 0x7f020081
-			public const int notification_template_icon_bg = 2130837633;
-			
-			// aapt resource value: 0x7f020082
-			public const int notification_template_icon_low_bg = 2130837634;
+			public const int notification_bg_normal_pressed = 2130837625;
 			
 			// aapt resource value: 0x7f02007a
-			public const int notification_tile_bg = 2130837626;
+			public const int notification_icon_background = 2130837626;
+			
+			// aapt resource value: 0x7f020082
+			public const int notification_template_icon_bg = 2130837634;
+			
+			// aapt resource value: 0x7f020083
+			public const int notification_template_icon_low_bg = 2130837635;
 			
 			// aapt resource value: 0x7f02007b
-			public const int notify_panel_notification_icon_bg = 2130837627;
+			public const int notification_tile_bg = 2130837627;
 			
 			// aapt resource value: 0x7f02007c
-			public const int profile3 = 2130837628;
+			public const int notify_panel_notification_icon_bg = 2130837628;
 			
 			// aapt resource value: 0x7f02007d
-			public const int round_magnify = 2130837629;
+			public const int profile3 = 2130837629;
 			
 			// aapt resource value: 0x7f02007e
-			public const int rounded_edittext = 2130837630;
+			public const int round_magnify = 2130837630;
 			
 			// aapt resource value: 0x7f02007f
-			public const int rounded_image = 2130837631;
+			public const int rounded_edittext = 2130837631;
 			
 			// aapt resource value: 0x7f020080
-			public const int roundVoiceButton = 2130837632;
+			public const int rounded_image = 2130837632;
+			
+			// aapt resource value: 0x7f020081
+			public const int roundVoiceButton = 2130837633;
 			
 			static Drawable()
 			{
