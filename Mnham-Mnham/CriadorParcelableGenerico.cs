83f75c4fc66f8b193de4db7082b6a8ff9556e2af
Joao Martins <a68646@alunos.uminho.pt>
2017-06-21 03:52:14 +0100

    Adicionei a classe CriadorParcelableGenerico que devia ter sido adicionada no commit anterior

---
+++ Mnham-Mnham/CriadorParcelableGenerico.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Android.OS;
+
+namespace Mnham_Mnham
+{
+    public sealed class CriadorParcelableGenerico<T> : Java.Lang.Object, IParcelableCreator
+        where T : Java.Lang.Object, new()
+    {
+        private readonly Func<Parcel, T> funCreate;
+
+        public CriadorParcelableGenerico(Func<Parcel, T> createFromParcelFunc)
+        {
+            funCreate = createFromParcelFunc;
+        }
+
+        #region IParcelableCreator Implementation
+
+        public Java.Lang.Object CreateFromParcel(Parcel source)
+        {
+            return funCreate(source);
+        }
+
+        public Java.Lang.Object[] NewArray(int size)
+        {
+            return new T[size];
+        }
+
+        #endregion
+    }
+}
